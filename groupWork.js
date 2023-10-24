f19b3e75e206bb30b312d1e8249d76193b89376f
raam <jeya.colombo@gmail.com>
2023-10-24 10:40:08 -0400

    Question A Completed

---
+++ groupWork.js
@@ -0,0 +1,30 @@
+// I.VARIABLE & DATA TYPES
+// A. Q+A
+
+// 1.How do we assign a value to a variable?
+
+// const variable = 10;
+
+// 2.How do we change the value of a variable?
+
+// variable = 20;
+
+// 3.How do we assign an existing variable to a new variable?
+
+// let newVariable = variable
+
+// 4.Remind me, what are declare, assign, and define?
+
+// Declare - create a variable using 'let', 'const' or 'var' keyword and a variable name without a value.
+// Assigning - Giving value to a variable after declaring it.
+// Define - Giving the variable its initial data type.
+
+// 5.What is pseudocoding and why should you do it?
+
+// Pseudocode is a way to write a description of a computer program or algorithm in plain english.
+// We use the Pseudocode to simplify and plan our program in small logical steps.
+
+// 6. What percentage of time should be spent thinking about
+// how you're going to solve a problem vs actually typing in code to solve it?
+
+//  A reasonable amount spend in planning and problem solving 50-70% and coding 50-30% in typing the actual code.
